a06ed7fc700c79a08ddd2cd498da3f17aa3158b1
oshen madhuka bhawajith <126389352+oshenmadhuka@users.noreply.github.com>
2024-05-02 12:11:51 +0530

    add to axios

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
 // Mock URL.createObjectURL
 window.URL.createObjectURL = function() {};
 
-test('renders learn react link', () => {
+test('renders learn react link', async () => {
   render(<App />);
-  const linkElement = screen.queryByText((content, element) => {
-    // Custom matcher to find text with case-insensitivity and trimmed whitespace
-    const normalizedText = element.textContent.trim().toLowerCase();
-    return normalizedText.includes('learn react'); // Using includes for flexibility
+  
+  // Wait for the element to appear
+  await waitFor(() => {
+    const linkElement = screen.queryByText((content, element) => {
+      const normalizedText = element.textContent.trim().toLowerCase();
+      return normalizedText.includes('learn react');
+    });
+    
+    expect(linkElement).toBeInTheDocument();
   });
-
-  expect(linkElement).toBeInTheDocument();
 });
