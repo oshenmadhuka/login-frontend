f837b299d7d32261b9008686f0aa3a8fd52cbd0a
oshen madhuka bhawajith <126389352+oshenmadhuka@users.noreply.github.com>
2024-05-02 11:53:17 +0530

    add to axios

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 // Mock URL.createObjectURL
-window.URL.createObjectURL = jest.fn(() => 'mocked-url');
+window.URL.createObjectURL = function() {};
+
 
 
 test('renders learn react link', () => {
